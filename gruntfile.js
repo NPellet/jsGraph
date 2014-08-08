c686348deae6d53860526f0ff9b80bb821aca63c
NPellet <norman.pellet@gmail.com>
2014-08-08 08:25:27 -0700

    Some releases just for fun

--- gruntfile.js
+++ gruntfile.js
@@ -74,7 +74,8 @@ module.exports = function(grunt) {
 
             options: {
                 files: ['package.json'],
-                updateConfigs: [ 'pkg' ]
+                updateConfigs: [ 'pkg' ],
+                push: false
             }
     
         }
