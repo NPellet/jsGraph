674d6484f0f9b7479566fb068b093c448bb17c05
NPellet <norman.pellet@gmail.com>
2014-08-21 15:22:21 -0700

    Update examples

---
+++ examples/listexamples.js
@@ -0,0 +1,36 @@
+
+define( function() {
+
+	var options = {};
+	var hashtag = (document.location.href.split("#")[ 1 ] || "").split(';').map( function( val ) {
+		val = val.split(':');
+		options[ val[ 0 ] ] = val[ 1 ];
+	});
+
+	var examples;
+	if( options.examples ) {
+
+		examples = options.examples.split(',').map( function( val ) { return './examples/src/' + val } );
+
+	} else {
+
+		examples = [
+			'./examples/src/basic',
+			'./examples/src/axis',
+			'./examples/src/contour',
+			'./examples/src/errorbars',
+			'./examples/src/2axis',
+			'./examples/src/legend',
+			'./examples/src/minmax',
+			'./examples/src/plugins',
+			'./examples/src/scatter',
+			'./examples/src/xinterval',
+			'./examples/src/shape'
+		];
+
+	}
+
+	window.examples = examples;
+	return examples;
+
+});
