4ba54e577a2b481836e3cf93cc559bc2e80d390a
NPellet <norman.pellet@gmail.com>
2017-11-07 20:40:35 +0100

    Release v2.0.49

--- dist/jsgraph.js
+++ dist/jsgraph.js
@@ -11649,7 +11649,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      'xmlns': Graph.ns,
 	      'font-family': this.options.fontFamily,
 	      'font-size': this.options.fontSize,
-	      'data-jsgraph-version': 'v2.0.48' || 'head'
+	      'data-jsgraph-version': 'v2.0.49' || 'head'
 	    });
 
 	    this.defs = document.createElementNS(Graph.ns, 'defs');
