14dbe9fbac8ab7f8b4da491cd291c749711f7336
NPellet <norman.pellet@gmail.com>
2017-11-15 10:28:22 +0100

    Release v2.0.56

--- dist/jsgraph-es6.js
+++ dist/jsgraph-es6.js
@@ -3630,7 +3630,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      'xmlns': Graph.ns,
 	      'font-family': this.options.fontFamily,
 	      'font-size': this.options.fontSize,
-	      'data-jsgraph-version': 'v2.0.55' || 'head'
+	      'data-jsgraph-version': 'v2.0.56' || 'head'
 	    });
 
 	    this.defs = document.createElementNS(Graph.ns, 'defs');
