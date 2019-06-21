d8c9866f5f0560728b7bd444618884d2f3db1aa8
Norman Pellet <norman.pellet@gmail.com>
2019-06-21 07:48:46 +0200

    Release v2.2.11

--- dist/jsgraph.js
+++ dist/jsgraph.js
@@ -12702,7 +12702,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
     try {
       util.setAttributeTo(this.dom, {
         // eslint-disable-next-line no-undef
-        'data-jsgraph-version': "v2.2.10"
+        'data-jsgraph-version': "v2.2.11"
       });
     } catch (e) {// ignore
     }
