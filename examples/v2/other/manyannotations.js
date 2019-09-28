b7a2f00f9ceda6b0c72c1d919eca9544b29851be
Norman Pellet <norman.pellet@gmail.com>
2019-09-28 10:49:49 +0200

    Add polygon

---
+++ examples/v2/other/manyannotations.js
@@ -0,0 +1,23 @@
+import Graph from '../../../src/graph.js';
+
+const domGraph = 'graph';
+
+var graph = new Graph( domGraph, {} );
+
+graph.resize( 400, 300 );
+
+const w = Graph.newWaveform();
+
+console.time('Serie');
+for( let i = 0; i < 10000; i ++ ) {
+  w.append(i, i);
+}
+const s = graph.newSerie( "mySerie", { markers: true }).autoAxis().setWaveform( w);
+s.setMarkerStyle( { shape: 'circle', r: 2, fill: 'rgba(255, 0, 0, 0.3)', stroke: 'rgb(255, 100, 0)' } );
+/*graph.makeLegend().notHideable().setAutoPosition('bottom');
+graph.getLegend().update();*/
+
+graph.autoScaleAxes();
+graph
+  .draw();
+ console.timeEnd('Serie');
