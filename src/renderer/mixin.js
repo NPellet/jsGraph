dc04bb1b266a2cf419cfc7f31758ec217c62e9e1
PELLET Norman <norman.pellet@csem.ch>
2020-11-02 21:34:28 +0100

    Fix #265

--- src/renderer/mixin.js
+++ src/renderer/mixin.js
@@ -1,6 +1,6 @@
 import graphFromJson from './main.js';
 
-export default ( Graph ) => {
+export default (Graph) => {
   /**
    * Returns a graph created from a schema
    * @param {Object} json
@@ -8,59 +8,60 @@ export default ( Graph ) => {
    * @param {Function} callback - A callback function called when something has changed, in the form of ( event, params... ) {}
    * @returns {Graph} Newly created graph
    */
-  Graph.fromJSON = ( json, wrapper, callback ) => {
+  Graph.fromJSON = (json, wrapper, callback) => {
     const options = json.options || {};
-    const graph = new Graph( undefined, options );
+    const graph = new Graph(undefined, options);
 
-    graphFromJson( Graph, graph, json, wrapper );
-    graph.setWrapper( wrapper );
+    graphFromJson(Graph, graph, json, wrapper);
+    graph.setWrapper(wrapper);
 
-    graph.onAll( function( eventName, ...args ) {
-      callback( eventName, ...args );
-    } );
-
+    if (callback) {
+      graph.onAll(function (eventName, ...args) {
+        callback(eventName, ...args);
+      });
+    }
     return graph;
   }
 
-  Graph.prototype.setJSON = ( json, options = {} ) => {
+  Graph.prototype.setJSON = (json, options = {}) => {
 
     // Destroy the current elements
     this.killSeries();
 
     const state = {};
-    if ( options.keepState ) {
+    if (options.keepState) {
 
-      this._applyToAxes( ( axis ) => {
+      this._applyToAxes((axis) => {
 
-        if ( axis.options.name ) {
+        if (axis.options.name) {
 
-          state[ axis.options.name ] = {
+          state[axis.options.name] = {
             min: axis.getCurrentMin(),
             max: axis.getCurrentMax()
           };
         }
-      }, undefined, true, true );
+      }, undefined, true, true);
     }
 
-    this._applyToAxes( ( axis ) => {
-      this.killAxis( axis, true, true );
-    }, undefined, true, true );
+    this._applyToAxes((axis) => {
+      this.killAxis(axis, true, true);
+    }, undefined, true, true);
 
     this.killLegend();
     this.killShapes();
 
-    graphFromJson( Graph, this, json );
+    graphFromJson(Graph, this, json);
 
-    if ( options.keepState ) {
+    if (options.keepState) {
 
-      this._applyToAxes( ( axis ) => {
+      this._applyToAxes((axis) => {
 
-        if ( axis.options.name && state[ axis.options.name ] ) {
+        if (axis.options.name && state[axis.options.name]) {
 
-          axis.setCurrentMin( state[ axis.options.name ].min );
-          axis.setCurrentMax( state[ axis.options.name ].max );
+          axis.setCurrentMin(state[axis.options.name].min);
+          axis.setCurrentMax(state[axis.options.name].max);
         }
-      }, undefined, true, true );
+      }, undefined, true, true);
     }
 
     this.draw();
