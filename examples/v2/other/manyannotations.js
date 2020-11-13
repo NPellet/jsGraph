28400c3ba21ebcc5c446f0e356423e6292bcfb91
PELLET Norman <norman.pellet@csem.ch>
2020-11-13 20:04:07 +0100

    Release v2.4.0

--- examples/v2/other/manyannotations.js
+++ examples/v2/other/manyannotations.js
@@ -2,22 +2,22 @@ import Graph from '../../../src/graph.js';
 
 const domGraph = 'graph';
 
-var graph = new Graph( domGraph, {} );
+var graph = new Graph(domGraph, { axes: { bottom: [{ type: 'time' }] } });
 
-graph.resize( 400, 300 );
+graph.resize(400, 300);
 
 const w = Graph.newWaveform();
 
 console.time('Serie');
-for( let i = 0; i < 10000; i ++ ) {
+for (let i = 0; i < 10000; i++) {
   w.append(i, i);
 }
-const s = graph.newSerie( "mySerie", { markers: true }).autoAxis().setWaveform( w);
-s.setMarkerStyle( { shape: 'circle', r: 2, fill: 'rgba(255, 0, 0, 0.3)', stroke: 'rgb(255, 100, 0)' } );
+const s = graph.newSerie("mySerie", { markers: true }, 'scatter').autoAxis().setWaveform(w);
+s.setMarkerStyle({ shape: 'circle', r: 2, fill: 'rgba(255, 0, 0, 0.3)', stroke: 'rgb(255, 100, 0)' });
 /*graph.makeLegend().notHideable().setAutoPosition('bottom');
 graph.getLegend().update();*/
 
 graph.autoScaleAxes();
 graph
   .draw();
- console.timeEnd('Serie');
+console.timeEnd('Serie');
--- src/renderer/axes.js
+++ src/renderer/axes.js
@@ -1,75 +1,77 @@
-const processAxes = ( Graph, graph, type, axisOptions, allAxes ) => {
+const processAxes = (Graph, graph, type, axisOptions, allAxes) => {
 
-  if ( !Array.isArray( axisOptions ) ) {
-    axisOptions = [ axisOptions ];
+  if (!Array.isArray(axisOptions)) {
+    axisOptions = [axisOptions];
   }
 
-  axisOptions.forEach( ( options ) => {
+  axisOptions.forEach((options) => {
     let constructorName;
 
-    if ( type == 'x' ) {
+    if (type == 'x') {
       type = 'bottom';
-    } else if ( type == 'y' ) {
+    } else if (type == 'y') {
       type = 'left';
     }
 
-    if ( type == 'bottom' || type == 'top' ) {
+    if (type == 'bottom' || type == 'top') {
       constructorName = 'graph.axis.x';
 
-      if ( options.type == 'category' ) {
+      if (options.type == 'category') {
         constructorName += '.bar';
+      } else if (options.type == 'time') {
+        constructorName += '.time';
       }
     } else {
       constructorName = 'graph.axis.y';
     }
 
-    var axis = new( Graph.getConstructor( constructorName ) )( graph, type );
-    axis.init( graph, options );
+    var axis = new (Graph.getConstructor(constructorName))(graph, type);
+    axis.init(graph, options);
 
-    if ( type == 'bottom' ) {
-      graph.setBottomAxis( axis, graph.getNumAxes( 'bottom' ) );
-    } else if ( type == 'top' ) {
-      graph.setTopAxis( axis, graph.getNumAxes( 'top' ) );
-    } else if ( type == 'left' ) {
-      graph.setLeftAxis( axis, graph.getNumAxes( 'left' ) );
-    } else if ( type == 'right' ) {
-      graph.setRightAxis( axis, graph.getNumAxes( 'right' ) );
+    if (type == 'bottom') {
+      graph.setBottomAxis(axis, graph.getNumAxes('bottom'));
+    } else if (type == 'top') {
+      graph.setTopAxis(axis, graph.getNumAxes('top'));
+    } else if (type == 'left') {
+      graph.setLeftAxis(axis, graph.getNumAxes('left'));
+    } else if (type == 'right') {
+      graph.setRightAxis(axis, graph.getNumAxes('right'));
     }
 
-    if ( options.type == 'category' ) {
+    if (options.type == 'category') {
       axis.categories = options.categories;
     }
 
-    if ( options.name ) {
-      allAxes[ name ] = axis;
+    if (options.name) {
+      allAxes[name] = axis;
     }
-  } );
+  });
 };
 
-const makeAxes = ( Graph, graph, jsonAxes ) => {
+const makeAxes = (Graph, graph, jsonAxes) => {
   const allAxes = [];
-  if ( jsonAxes.x ) {
-    processAxes( Graph, graph, 'x', jsonAxes.x, allAxes );
+  if (jsonAxes.x) {
+    processAxes(Graph, graph, 'x', jsonAxes.x, allAxes);
   }
 
-  if ( jsonAxes.y ) {
-    processAxes( Graph, graph, 'y', jsonAxes.y, allAxes );
+  if (jsonAxes.y) {
+    processAxes(Graph, graph, 'y', jsonAxes.y, allAxes);
   }
 
-  if ( jsonAxes.top ) {
-    processAxes( Graph, graph, 'top', jsonAxes.top, allAxes );
+  if (jsonAxes.top) {
+    processAxes(Graph, graph, 'top', jsonAxes.top, allAxes);
   }
 
-  if ( jsonAxes.left ) {
-    processAxes( Graph, graph, 'left', jsonAxes.left, allAxes );
+  if (jsonAxes.left) {
+    processAxes(Graph, graph, 'left', jsonAxes.left, allAxes);
   }
 
-  if ( jsonAxes.bottom ) {
-    processAxes( Graph, graph, 'bottom', jsonAxes.bottom, allAxes );
+  if (jsonAxes.bottom) {
+    processAxes(Graph, graph, 'bottom', jsonAxes.bottom, allAxes);
   }
 
-  if ( jsonAxes.right ) {
-    processAxes( Graph, graph, 'right', jsonAxes.right, allAxes );
+  if (jsonAxes.right) {
+    processAxes(Graph, graph, 'right', jsonAxes.right, allAxes);
   }
 };
 
