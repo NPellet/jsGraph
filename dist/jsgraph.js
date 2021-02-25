84356748c45205cc246f16774a9fb37aaf350e57
Michaël Zasso <targos@protonmail.com>
2021-02-25 11:36:07 +0100

    Release v2.4.5

--- dist/jsgraph.js
+++ dist/jsgraph.js
@@ -8062,7 +8062,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
     try {
       util.setAttributeTo(this.dom, {
         // eslint-disable-next-line no-undef
-        'data-jsgraph-version': "v2.4.4"
+        'data-jsgraph-version': "v2.4.5"
       });
     } catch (e) {// ignore
     }
